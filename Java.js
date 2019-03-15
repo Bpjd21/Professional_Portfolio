3752cfcaf8a1dca4a67f991b2645ea70df65251a
Brian <bpjd21@yahoo.com>
2019-03-15 18:43:08 -0400

    added more CSS and Nav Bar Responsiveness

--- Java.js
+++ Java.js
@@ -4,35 +4,43 @@ const carouselImages = document.querySelectorAll('.carousel-slide img');
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 
-//Counter
 let counter = 1;
 const size = carouselImages[0].clientWidth;
 
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
-nextBtn.addEventListerner('onclick', () => {
+nextBtn.addEventListener('click', () => {
     if (counter >= carouselImages.length -1) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 })
 
-prevBtn.addEventListerner('click', () => {
+prevBtn.addEventListener('click', () => {
     if (counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 })
 
 carouselSlide.addEventListener('transitionend', () => {
-    if (carouselImages[counter].id === 'lastClone'){
+    if (carouselImages[counter].id === 'lastclone'){
         carouselSlide.style.transition = "none";
         counter = carouselImages.length -2;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     } 
-    if (carouselImages[counter].id === 'firstClone'){
+    if (carouselImages[counter].id === 'firstclone'){
         carouselSlide.style.transition = "none";
         counter = carouselImages.length - counter;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }    
 })
+
+/*function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }*/
